72ac6a8d54b7727eed93508fec53522897ea43fa
Greg Carter <greg.a.carter1@gmail.com>
2023-12-28 16:39:23 -0500

    fixed Links

--- store/index.js
+++ store/index.js
@@ -3,4 +3,4 @@ export { default as Contact } from "./Contact";
 export { default as Instructor } from "./Instructor";
 export { default as Student } from "./Student";
 export { default as Technique } from "./Technique";
-export { default as Links } from "./links";
+export { default as Links } from "./Links";
