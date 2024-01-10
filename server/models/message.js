fbffb1722eace7667d511a2c3b51b15ffb583c5b
Greg Carter <greg.a.carter1@gmail.com>
2024-01-09 21:05:47 -0500

    backend

---
+++ server/models/message.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const contactSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+    validate: /^[A-Za-z0-9 ]*$/
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function(v) {
+        return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+          v
+        );
+      },
+      message: props => `${props.value} is not a valid email address!`
+    }
+  },
+  phone: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function(v) {
+        return /^(\()?\d{3}(\))?(-|\s)?\d{3}(-|\s)\d{4}$/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }
+  },
+  message: {
+    type: String,
+    required: true,
+    validate: /^[A-Za-z0-9 ]*$/
+  }
+});
+
+const Contact = mongoose.model("Contact", contactSchema);
+
+export default Contact;
